657441308a91293254fca2ef4692238f230ad93e
Yo Vannaravuth <ravuthz@gmail.com>
2017-10-06 16:38:16 +0000

    React Rotuer setup with few pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
 class App extends Component {
   render() {
@@ -13,6 +17,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <Route path="/" exact component={HomePage}/>
+          <Route path="/login" exact component={LoginPage}/>
+        </div>
       </div>
     );
   }

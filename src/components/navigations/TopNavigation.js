893d7081b5800066a365161daa2d4f41c1d2ad57
Ravuthz <ravuthz@gmail.com>
2017-11-11 17:41:06 +0700

    Change TopNavigation

--- src/components/navigations/TopNavigation.js
+++ src/components/navigations/TopNavigation.js
@@ -1,24 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Menu, Dropdown, Image } from 'semantic-ui-react';
+import { Container, Menu, Dropdown, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import gravatarUrl from 'gravatar-url';
 
+import logo from '../../logo.svg';
 import * as actions from '../../actions/auth';
 
 const TopNavigation = ({ user, logout }) => (
-  <Menu secondary pointing>
-    <Menu.Item as={Link} to="/dashboard">
-      Dashboard
-    </Menu.Item>
-    <Menu.Menu position="right">
-      <Dropdown trigger={<Image avatar src={gravatarUrl(user.email)} />}>
+  <Menu fixed="top" inverted>
+    <Container>
+      <Menu.Item as="a" header>
+        <Image size="mini" src={logo} style={{ marginRight: '1.5em' }} />
+        Project Name
+      </Menu.Item>
+      <Menu.Item as={Link} to="/dashboard">
+        Dashboard
+      </Menu.Item>
+      <Dropdown item simple text="Dropdown">
         <Dropdown.Menu>
-          <Dropdown.Item onClick={() => logout()}>Logout</Dropdown.Item>
+          <Dropdown.Item>List Item</Dropdown.Item>
+          <Dropdown.Item>List Item</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Header>Header Item</Dropdown.Header>
+          <Dropdown.Item>
+            <i className="dropdown icon" />
+            <span className="text">Submenu</span>
+            <Dropdown.Menu>
+              <Dropdown.Item>List Item</Dropdown.Item>
+              <Dropdown.Item>List Item</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown.Item>
+          <Dropdown.Item>List Item</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
-    </Menu.Menu>
+      <Menu.Menu position="right">
+        <Dropdown
+          trigger={<Image avatar src={gravatarUrl(user.email)} />}
+          style={{ marginTop: '12px', color: 'white' }}
+        >
+          <Dropdown.Menu>
+            <Dropdown.Item onClick={() => logout()}>Logout</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Menu.Menu>
+    </Container>
   </Menu>
 );
 
